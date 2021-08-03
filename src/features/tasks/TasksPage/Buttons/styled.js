f7a381f8805d563dc4b865e05f7e81bbc24cb334
palel <32599663+palel@users.noreply.github.com>
2021-08-03 09:38:07 +0200

    stylled tasks button moved to separate file

--- src/features/tasks/TasksPage/Buttons/styled.js
+++ src/features/tasks/TasksPage/Buttons/styled.js
@@ -8,25 +8,3 @@ export const Wrapper = styled.div`
         grid-template-columns: 1fr;
     }
 `;
-
-export const Button = styled.button`
-    background-color: ${({ theme }) => theme.color.white};
-    color: ${({ theme }) => theme.color.teal};
-    border-style: none;
-    padding: 15px 15px;
-    transition: 0.3s;
-
-    &:hover:not([disabled]) {
-        filter: brightness(110%);
-        cursor: pointer;
-    }
-
-    &:active:not([disabled]) {
-        filter: brightness(120%);
-    }
-
-    &:disabled {
-        color: ${({ theme }) => theme.color.silver};
-        cursor: not-allowed;
-    }
-`;
